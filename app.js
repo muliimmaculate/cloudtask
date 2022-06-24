e9761a8ec62a67686991fb353086a2ef4b03278f
muliimmaculate <muliimaculate@gmail.com>
2022-06-24 17:26:10 +0300

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 function myFunction ();
-var un = document.forms["myForm"]["Uname"].value;
-var pw = document.forms["myForm"]["pass"].value;
-
-if(un=="admin" && pw=="123456")
 {
   window.location.href="about.html"
-}
-else{
-  alert("invalid username and password");
 }
